e383b48231738b139d56ac0bd764a9e07eebfd40
jasonu95 <jasonu95@gmail.com>
2024-09-23 00:00:17 -0600

    Se agrega mp3

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,12 @@
 onload = () =>{
     document.body.classList.remove("container");
 };
+
+// Seleccionamos el elemento div con clase "message" y el elemento de audio
+const messageDiv = document.querySelector('.message');
+const myAudio = document.getElementById('myAudio');
+
+// Añadimos un evento de clic al div para reproducir el audio
+messageDiv.addEventListener('click', function() {
+    myAudio.play();
+});
