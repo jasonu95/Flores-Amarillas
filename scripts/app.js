66452ec7ded15bd00597b1899390b3047b5ca38d
jasonu95 <jasonu95@gmail.com>
2024-10-12 15:04:20 -0600

    Versión 2

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-onload = () =>{
+/* onload = () =>{
     document.body.classList.remove("container");
 };
 
@@ -9,4 +9,23 @@ const myAudio = document.getElementById('myAudio');
 // Añadimos un evento de clic al div para reproducir el audio
 messageDiv.addEventListener('click', function() {
     myAudio.play();
-});
+}); */
+
+
+// Al cargar la página, se esconde el contenido principal
+window.onload = () => {
+    document.getElementById('playButton').addEventListener('click', () => {
+        // Desvanecer la pantalla de inicio
+        const introScreen = document.getElementById('introScreen');
+        introScreen.classList.add('fade-out');
+        
+        // Mostrar el contenido principal después de la animación
+        setTimeout(() => {
+            introScreen.style.display = 'none';
+            document.getElementById('mainContent').style.display = 'block';
+
+            // Iniciar la animación y la música
+            document.getElementById('myAudio').play();
+        }, 1000); // Coincide con la duración de la animación de desvanecimiento
+    });
+};
